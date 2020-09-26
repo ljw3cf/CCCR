a9a35af549b048415b9784e72c44ca4a6fa57d95
ljwkeke <ljw3cf@naver.com>
2020-09-26 20:11:45 +0900

    add project/Voice-s3.py for Mr.kang

---
+++ project/java-sdk/ljwkeke/src/test/java/ljwkeke/AppTest.java
@@ -0,0 +1,20 @@
+package ljwkeke;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
